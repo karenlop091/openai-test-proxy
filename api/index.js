7d3e0274b267366d768ea05c7c8783249756f03e
karenlop091 <34432163+karenlop091@users.noreply.github.com>
2025-11-11 10:26:01 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ export default function handler(req, res) {
 
   res.status(200).json({
     key: Math.random().toString(36).substring(2, 10) + '-' + Math.random().toString(36).substring(2, 10),
-    expiresInHours: 24,
-    tokensLeft: 10000,
+    expiresInHours: 170,
+    tokensLeft: 15000,
     name
   });
 }
